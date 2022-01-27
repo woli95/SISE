c4778ae781748e01400e49024e3c969c0f19b1ff
woli95 <wolszczakd@gmail.com>
2022-01-27 16:04:02 +0100

    x

--- removeResultFiles.py
+++ removeResultFiles.py
@@ -1,8 +1,9 @@
 import glob
 import os
 
-stats = glob.glob('./413ukladow/*dfs*stats.txt')
-sol = glob.glob('./413ukladow/*dfs*sol.txt')
+
+stats = glob.glob('./413ukladow/*stats.txt')
+sol = glob.glob('./413ukladow/*sol.txt')
 
 for file in stats:
     os.remove(file)
